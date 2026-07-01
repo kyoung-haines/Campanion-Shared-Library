33a1a63d1dc86b4cda84ec44230317bff9c6e3fb
kyoung-haines <k.younghaines@gmail.com>
2026-07-01 12:21:25 -0400

    Add project files.

---
+++ Campanion.Shared/Dtos/TripDtos/CreateTripDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Campanion.Shared.Dtos.TripDtos
+{
+    public class CreateTripDto
+    {
+        [Required]
+        public string TripName { get; set; }
+        [Required]
+        public DateOnly TripStartDate { get; set; }
+        [Required]
+        public DateOnly TripEndDate { get; set; }
+        public DateTime TripCreatedAt { get; set; }
+
+
+        // this will need to refactored
+        // this should be a list of users (friends) that have been added
+        // it won't be strings I don't think
+        public List<string> TripAttendees { get; set; }
+    }
+}
