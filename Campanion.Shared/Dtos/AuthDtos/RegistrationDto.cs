daf38c223afe758d03bc8f06d649ac630551cce9
kyoung-haines <k.younghaines@gmail.com>
2026-07-08 13:53:16 -0400

    Added DTO properties

--- Campanion.Shared/Dtos/AuthDtos/RegistrationDto.cs
+++ Campanion.Shared/Dtos/AuthDtos/RegistrationDto.cs
@@ -6,5 +6,17 @@ namespace Campanion.Shared.Dtos.AuthDtos
 {
     public class RegistrationDto
     {
+        public string AppUserId = string.Empty;
+        public string AppUserType = string.Empty;
+        public string AppUserUsername = string.Empty;
+        public string AppUserPassword = string.Empty;
+        public string AppUserEmail = string.Empty;
+        public string AppUserPhone = string.Empty;
+        public string AppUserFirstName = string.Empty;
+        public string AppUserLastName = string.Empty;
+        public string AppUserStreetAddress = string.Empty;
+        public string AppUserCity = string.Empty;
+        public string AppUserProvince = string.Empty;
+        public string AppUserPostalCode = string.Empty;
     }
 }
