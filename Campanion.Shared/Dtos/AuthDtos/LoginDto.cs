63fc71f3e5a7039d78c96e8f726bf14aa6a347c6
kyoung-haines <k.younghaines@gmail.com>
2026-07-08 12:15:35 -0400

    Added shared DTO for front-end and backend sharing

---
+++ Campanion.Shared/Dtos/AuthDtos/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Campanion.Shared.Dtos.AuthDtos
+{
+    public class LoginDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
