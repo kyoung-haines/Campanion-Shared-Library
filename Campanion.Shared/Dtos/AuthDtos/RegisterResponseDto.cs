687adb15f4cc8fadfa0f03fedddc3122667f3fbe
kyoung-haines <k.younghaines@gmail.com>
2026-07-08 15:29:07 -0400

    Added DTO skeleton

---
+++ Campanion.Shared/Dtos/AuthDtos/RegisterResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using Campanion.Shared.Dtos.ProfileDtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Campanion.Shared.Dtos.AuthDtos
+{
+    public class RegisterResponseDto
+    {
+        public string Token { get; set; }
+        public ProfileResponseDto Profile { get; set; }
+    }
+}
---
+++ Campanion.Shared/Dtos/ProfileDtos/CreateProfileDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Campanion.Shared.Dtos.ProfileDtos
+{
+    public class CreateProfileDto
+    {
+    }
+}
