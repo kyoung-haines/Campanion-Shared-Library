b207af6ef34d662c1bbf388e9c12a0a8b004a977
kyoung-haines <k.younghaines@gmail.com>
2026-07-09 14:10:16 -0400

    Added member properties

--- Campanion.Shared/Dtos/ProfileDtos/ProfileResponseDto.cs
+++ Campanion.Shared/Dtos/ProfileDtos/ProfileResponseDto.cs
@@ -6,5 +6,10 @@ namespace Campanion.Shared.Dtos.ProfileDtos
 {
     public class ProfileResponseDto
     {
+        public string ProfileId { get; set; }
+        public string ProfileUsername { get; set; }
+        public string ProfileImagePath { get; set; }
+        public string ProfileCreatedAt { get; set; }
+        public string AppUserId { get; set; }
     }
 }
