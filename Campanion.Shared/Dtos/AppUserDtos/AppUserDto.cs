1699c7c181a401feddfad0a68b3f4162b6b5f658
kyoung-haines <k.younghaines@gmail.com>
2026-07-30 16:18:50 -0400

    Added properties

---
+++ Campanion.Shared/Dtos/AppUserDtos/AppUserDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Campanion.Shared.Dtos.AppUserDtos
+{
+    public class AppUserDto()
+    {
+        public string AppUserEmail { get; set; }
+        public string AppUserPhone { get; set; }
+        public string AppUserType { get; set; }
+        public string AppUserFirstName { get; set; }
+        public string AppUserLastName { get; set; }
+        public string AppUserStreetAddress { get; set; }
+        public string AppUserCity { get; set; }
+        public string AppUserProvince { get; set; }
+        public string AppUserCountry { get; set; }
+        public string AppUserPostalCode { get; set; }  
+        public List<string> AppUserFavouriteCampgrounds { get; set; }
+    }
+}
