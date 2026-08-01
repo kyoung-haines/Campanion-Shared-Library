e02688aa590930ef3257f562131d80b7d37df3bc
kyoung-haines <k.younghaines@gmail.com>
2026-08-01 10:39:20 -0400

    Added properties

--- Campanion.Shared/Dtos/ProfileDtos/ProfileResponseDto.cs
+++ Campanion.Shared/Dtos/ProfileDtos/ProfileResponseDto.cs
@@ -8,6 +8,8 @@ namespace Campanion.Shared.Dtos.ProfileDtos
     {
         public string ProfileId { get; set; }
         public string ProfileUsername { get; set; }
+        public string ProfileUserProvince { get; set; }
+        public string ProfileUserCountry { get; set; }
         public string ProfileImagePath { get; set; }
         public string ProfileCreatedAt { get; set; }
         public string AppUserId { get; set; }
