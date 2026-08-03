a6674f91aa39f25abe3009d1795338bc055d7943
kyoung-haines <k.younghaines@gmail.com>
2026-08-03 10:51:19 -0400

    Added new DTOs for favourite campgrounds

---
+++ Campanion.Shared/Dtos/CampgroundDtos/FavouriteCampgroundDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Campanion.Shared.Dtos.CampgroundDtos
+{
+    public class FavouriteCampgroundDto
+    {
+        public string CampgroundId { get; set; }
+        public string CampgroundName { get; set; }
+        public string CampgroundImagePath { get; set; }
+    }
+}
---
+++ Campanion.Shared/Dtos/CampgroundDtos/FavouriteCampgroundsDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Campanion.Shared.Dtos.CampgroundDtos
+{
+    public class FavouriteCampgroundsDto
+    {
+        public List<FavouriteCampgroundDto> FavouriteCampgrounds { get; set; } = new();
+
+        public async Task AddFavouriteCampgroundDto(FavouriteCampgroundDto favCampgroundDto)
+        {
+            this.FavouriteCampgrounds.Add(favCampgroundDto);
+        }
+    }
+}
