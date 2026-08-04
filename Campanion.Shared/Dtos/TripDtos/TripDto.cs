49b48a8b97f44d36aaaf7fb1a4626d892a819e5c
kyoung-haines <k.younghaines@gmail.com>
2026-08-04 11:10:15 -0400

    Added for Trip entity data transfer

---
+++ Campanion.Shared/Dtos/TripDtos/TripDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Campanion.Shared.Dtos.TripDtos
+{
+    public class TripDto
+    {
+        public string TripId { get; set; }
+        public string? TripName { get; set; }
+        public string TripStatus { get; set; }
+        public string TripStartDate { get; set; }
+        public string TripEndDate { get; set; }
+        public string TripCreationDate { get; set; }
+        public List<object> TripAttendees { get; set; }
+    }
+}
