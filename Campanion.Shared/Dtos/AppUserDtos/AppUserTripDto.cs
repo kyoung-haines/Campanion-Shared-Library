2c663c4120c16c916994c22991f1f858e912bcff
kyoung-haines <k.younghaines@gmail.com>
2026-08-10 15:03:21 -0400

    Added DTO

---
+++ Campanion.Shared/Dtos/AppUserDtos/AppUserTripDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Campanion.Shared.Dtos.AppUserDtos
+{
+    public class AppUserTripDto
+    {
+        public string AppUserIdDto { get; set; }
+        public string TripIdDto { get; set; } 
+    }
+}
